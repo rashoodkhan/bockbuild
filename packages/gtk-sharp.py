f6b38ce5a82fd07a4a52e98ab4a4084a425e9441
David Nielsen <gnomeuser@gmail.com>
2011-12-07 16:18:17 -0200

    Update gtk-sharp to ae2a95c77721d8e57d51f17f84fa73ed0d8478ef on the gtk-sharp 2.12 branch

--- packages/gtk-sharp.py
+++ packages/gtk-sharp.py
@@ -1,7 +1,7 @@
 class GtkSharpPackage (Package):
 	def __init__ (self):
 		Package.__init__ (self, 'gtk-sharp', '2-12-branch')
-		self.commit = 'b078aacaf263af84605812d778b62afbdf3e1b59'
+		self.commit = 'ae2a95c77721d8e57d51f17f84fa73ed0d8478ef'
 		self.source_dir_name = 'mono-gtk-sharp-%s' % self.commit[:7]
 		self.configure = './bootstrap-2.12 --prefix="%{prefix}"'
 		self.sources = [

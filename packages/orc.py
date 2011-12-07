ad0c3fee25e9f16b11a8ec88bda5bfca3d8e6a40
David Nielsen <gnomeuser@gmail.com>
2011-12-07 11:28:42 -0200

    Syntax improvements to orc package

--- packages/orc.py
+++ packages/orc.py
@@ -1,5 +1,5 @@
 Package ('orc', '0.4.16',
 	sources = [
-		'http://code.entropywave.com/download/orc/orc-0.4.16.tar.gz'
+		'http://code.entropywave.com/download/%{name}/%{name}-%{version}.tar.gz'
 	]
 )

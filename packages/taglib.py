2edd997562e03b3b0b7e93dbf995dbb9ee7137eb
David Nielsen <gnomeuser@gmail.com>
2011-12-07 14:14:39 -0200

    Update taglib to 1.7

--- packages/taglib.py
+++ packages/taglib.py
@@ -1,3 +1,12 @@
-Package ('taglib', '1.6.3', sources = [
-	'http://developer.kde.org/~wheeler/files/src/%{name}-%{version}.tar.gz'
-])
+Package ('taglib', '1.7', 
+	sources = [
+		'http://developer.kde.org/~wheeler/files/src/%{name}-%{version}.tar.gz'
+	],
+	override_properties = {
+		'configure': './autogen.sh ; cmake '
+			'[-DCMAKE_INSTALL_PREFIX=%{prefix}] '
+			'[-Dlibdir=%{prefix}/lib] '
+			'[-Ddatadir=%{prefix}/share] '
+			'.'
+	}
+)

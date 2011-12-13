8f8c376ada1c793459dccb202a34bf1322b57de3
David Nielsen <gnomeuser@gmail.com>
2011-12-13 09:51:15 -0200

    Upate libgcrypt to 1.5.0, add --disable-asm to allow compilation with xcode 4.2.1

--- packages/libgcrypt.py
+++ packages/libgcrypt.py
@@ -1,3 +1,5 @@
-Package ('libgcrypt', '1.4.6', sources = [
+Package ('libgcrypt', '1.5.0', sources = [
 	'ftp://ftp.gnupg.org/gcrypt/%{name}/%{name}-%{version}.tar.bz2'
-])
+],
+	configure_flags = [ '--disable-asm' ]
+)

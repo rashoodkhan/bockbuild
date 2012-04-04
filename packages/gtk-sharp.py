46cd42f2f03af2f092760eec266f1a1b82757e85
dynalon <timo.doerr@latecrew.de>
2012-04-04 18:05:02 +0200

    Use fixed gtk-sharp branch from github

--- packages/gtk-sharp.py
+++ packages/gtk-sharp.py
@@ -1,11 +1,11 @@
 class GtkSharpPackage (Package):
 	def __init__ (self):
 		Package.__init__ (self, 'gtk-sharp', '2-12-branch')
-		self.commit = 'ae2a95c77721d8e57d51f17f84fa73ed0d8478ef'
-		self.source_dir_name = 'mono-gtk-sharp-%s' % self.commit[:7]
+		self.commit = 'a0d89be14edb8da9e1654c57a490689b19dea4fd'
+		self.source_dir_name = 'Dynalon-gtk-sharp-%s' % self.commit[:7]
 		self.configure = './bootstrap-2.12 --prefix="%{prefix}"'
 		self.sources = [
-			'http://github.com/mono/gtk-sharp/tarball/%{commit}'
+			'http://github.com/Dynalon/gtk-sharp/tarball/%{commit}'
 		]
 
 GtkSharpPackage ()

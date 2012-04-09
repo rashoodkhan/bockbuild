7ceaeb79069edea5c2e2cd9107959af5712ca38d
dynalon <timo.doerr@latecrew.de>
2012-04-09 22:14:01 +0200

    Fallback to 1.4.6 for Xcode 3.x on Snow Leopard

--- packages/libgcrypt.py
+++ packages/libgcrypt.py
@@ -1,5 +1,15 @@
-Package ('libgcrypt', '1.5.0', sources = [
-	'ftp://ftp.gnupg.org/gcrypt/%{name}/%{name}-%{version}.tar.bz2'
-],
-	configure_flags = [ '--disable-asm' ]
-)
+class LibgcryptPackage (Package):
+	def __init__ (self):
+		Package.__init__ (self, 'libgcrypt', '1.5.0')
+
+		if Package.profile.name == 'darwin' and Package.profile.os_x_minor >= 7:
+			self.version = '1.5.0'
+			self.configure_flags.extend(['--disable-asm'])
+
+		# 1.5.0 does not compile with Xcode 3.x on Snow Leopard and Xcode4 is Lion only
+		if Package.profile.name == 'darwin' and Package.profile.os_x_minor == 6:
+			self.version = '1.4.6'
+		
+		self.sources = ['ftp://ftp.gnupg.org/gcrypt/%{name}/%{name}-%{version}.tar.bz2']
+		
+LibgcryptPackage ()

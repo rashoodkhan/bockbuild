4b4785470229d1458938e8ef73201ef2757cae7c
Timo Dörr <timo@latecrew.de>
2012-06-05 12:54:56 +0200

    [bockbuild] allow git packages to specify a branch
    Packages using git sources can now specify a custom git branch by
    setting:
    
    	self.git_branch = 'mybranch'
    
    in the package definition file. If no branch is set, it defaults to
    'master'
    
    Conflicts:
    
    	bockbuild/package.py

--- bockbuild/package.py
+++ bockbuild/package.py
@@ -32,6 +32,7 @@ class Package:
 		self.configure = './configure --prefix="%{prefix}"'
 		self.make = 'make -j%s' % Package.profile.cpu_count
 		self.makeinstall = 'make install'
+		self.git_branch = 'master'
 		self.git = 'git'
 		for git in ['/usr/bin/git', '/usr/local/bin/git', '/usr/local/git/bin/git']:
 			if os.path.isfile (git):
@@ -71,8 +72,7 @@ class Package:
 				if not os.path.isdir (os.path.join (local_dest_file, '.git')):
 					self.cd (os.path.dirname (local_dest_file))
 					shutil.rmtree (local_dest_file, ignore_errors = True)
-					self.sh ('%' + '{git} clone "%s" "%s"' % (source, os.path.basename (local_dest_file)))
-				else:
+					self.sh ('%' + '{git} clone -b %s "%s" "%s"' % (self.git_branch, source, os.path.basename (local_dest_file)))
 					self.cd (local_dest_file)
 					self.sh ('%{git} pull --rebase')
 				os.chdir (pwd)

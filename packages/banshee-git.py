33278fbda9b59bf940c8c79cd1723f7c46486de2
Timo Dörr <timo@latecrew.de>
2012-07-17 18:59:27 +0200

    [banshee-git] bump version, add patches
    Add some patches to bring banshee to latest state

--- packages/banshee-git.py
+++ packages/banshee-git.py
@@ -9,10 +9,24 @@ class BansheePackage (Package):
 
 		self.configure = [ 'NOCONFIGURE=1 ./autogen.sh && ./profile-configure %{profile.name} --prefix=%{prefix}' ]
 		self.sources.extend([
+			'https://github.com/Dynalon/banshee-osx/pull/1.diff',
+
+			# https://bugzilla.gnome.org/show_bug.cgi?id=677866
+			'http://bugzilla-attachments.gnome.org/attachment.cgi?id=217548',
+
+			# watch out: this patch might not make it into master
+			# see the bgo report at
+			# https://bugzilla.gnome.org/show_bug.cgi?id=679252
+			'http://bugzilla-attachments.gnome.org/attachment.cgi?id=217821',
+
+			# code cleanup, remove VolumeButton and use gtk
+			# https://bugzilla.gnome.org/show_bug.cgi?id=681062
+			'http://bugzilla-attachments.gnome.org/attachment.cgi?id=220140',
 		])
 
 	def prep (self):
 		Package.prep (self)
-		#self.sh ('patch -p1 < %{sources[1]}')
+		for p in range (1, len (self.sources)):
+			self.sh ('patch -p1 < "%{sources[' + str (p) + ']}"')
 
 BansheePackage ()
